dfdbc7e6f87b7e72fac97822f5d43fc2bb1571f5
LinghtSun <532278976@qq.com>
2016-01-18 16:33:22 +0800

    add swipeListView from third

---
+++ common-utils/swipelistview/src/androidTest/java/study/heaven7/com/swipelistview/ApplicationTest.java
@@ -0,0 +1,13 @@
+package study.heaven7.com.swipelistview;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
